4afd47f4ac7702ed5e2a91b533ab11567bbe7f47
Tyler Rapson <rapson95@gmail.com>
2016-07-27 14:21:02 -0500

    Step 1-7 of Peer challenge "Talk it out"

--- peer_JS2_01.js
+++ peer_JS2_01.js
@@ -19,12 +19,12 @@
 
 8. Add 'value' and the new variable created in Step 6 together and store them in 'value'
 
-9. If the new value of 'value' is less than 60, set 'value' equal to 14. If not, check to see if it is equal to 2930, if it is, set 'value' equal to 27. 
+9. If the new value of 'value' is less than 60, set 'value' equal to 14. If not, check to see if it is equal to 2930, if it is, set 'value' equal to 27.
 If neither of these are true, set the value to 2.
 
 10. Create a while loop that counts down from 10 and increments 'value' by 1.
 
-11. Create a function that accepts an argument 'val'. The function should convert 'val' to a String, then drop the first character off the String, but only if there 
+11. Create a function that accepts an argument 'val'. The function should convert 'val' to a String, then drop the first character off the String, but only if there
 is more than 1 character in the String. Return 'val' and set the 'value' to the returned value.
 
 12. Call the function.
@@ -33,3 +33,37 @@ is more than 1 character in the String. Return 'val' and set the 'value' to the 
 
 14. Your answer should be a String value that equals 7. Is that what you got?
 */
+//Step 1
+var value=42
+//Step 2
+if(value>=53){
+   value += 42;
+ }else{
+   value -= 13;
+ }
+ //Step 3
+ value += "11";
+ console.log(value);
+ //Step 4
+var arrayA=[]
+for(i=0; i<value.length;i++){
+  arrayA[i]=value.charAt(i);
+}
+console.log(arrayA);
+//Step 5
+arrayA.shift();
+arrayA.pop();
+console.log(arrayA);
+//Step 6
+var newValue= '';
+for(i=arrayA.length;i>0;i--){
+  //console.log("suhhh")
+  newValue+=arrayA.pop();
+}
+
+console.log(newValue);
+//Step 7
+value = parseInt(value);
+newValue = parseInt(newValue);
+console.log(typeof(value));
+console.log(typeof(newValue));
